0baff37eda738306e01d00a22d9f081a029b2ce1
bigfishh <anniezheng.anniee@gmail.com>
2021-05-22 13:56:20 -0400

    adds notes

---
+++ part2/notes/src/App.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import './App.css';
+import {useState, useEffect} from 'react'
+
+function App() {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then((response) => {
+        setNotes(response.data)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
